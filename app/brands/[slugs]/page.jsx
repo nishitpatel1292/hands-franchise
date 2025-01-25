ef988b07565bd2d2427e18f1389f1720b9d2f276
nishitpatel1292 <73834549+nishitpatel1292@users.noreply.github.com>
2025-01-25 12:50:19 +0530

    add favicon, style franchise page headings

--- app/brands/[slugs]/page.jsx
+++ app/brands/[slugs]/page.jsx
@@ -7,6 +7,9 @@ import {
   TrendingUp,
   Calendar1Icon,
   IndianRupee,
+  MapPin,
+  Store,
+  GalleryHorizontalEnd,
 } from "lucide-react";
 import Image from "next/image";
 
@@ -16,13 +19,19 @@ const franchiseData = {
     name: "Hocco Eatery",
     hero: "/brands/hocco-eatery.png",
     description: "India's fastest growing fastfood chain",
-    yearsManaged: "2.5Yrs+",
+    startDate: "15/04/2022",
     investment: "₹2Cr",
     revenue: "₹5Cr average annual turnover",
     employeeCount: "4-5 per location",
     locations: [
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.6319891508265!2d72.5061748!3d23.0005564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9bd5f9a646db%3A0x5ff37f40cb89fa8b!2sHocco%20Eatery!5e0!3m2!1sen!2sin!4v1737394136379!5m2!1sen!2sin",
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58694.16674258091!2d72.59302097977942!3d23.15612909431003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e8194a7411881%3A0x739b636940d1ddaa!2shocco%20eatry!5e0!3m2!1sen!2sin!4v1737394656651!5m2!1sen!2sin",
+      {
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.6319891508265!2d72.5061748!3d23.0005564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9bd5f9a646db%3A0x5ff37f40cb89fa8b!2sHocco%20Eatery!5e0!3m2!1sen!2sin!4v1737394136379!5m2!1sen!2sin",
+        locationName: "Makarba",
+      },
+      {
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58694.16674258091!2d72.59302097977942!3d23.15612909431003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e8194a7411881%3A0x739b636940d1ddaa!2shocco%20eatry!5e0!3m2!1sen!2sin!4v1737394656651!5m2!1sen!2sin",
+        locationName: "Bhaijipura",
+      },
     ],
     gallery: [
       "/franchises/hocco eatery/hocco_eatery_1.jpg",
@@ -38,8 +47,13 @@ const franchiseData = {
     hero: "/brands/hocco-ice-cream.jpg",
     description:
       "We are the distributors of India's fastest growing icecream chain.",
-    yearsManaged: "1.2Yrs+",
-    locations: ["https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117524.7896871604!2d72.3536168972656!3d22.99990630000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9b00149f940d%3A0x7f561de6c03092fb!2sH%20%26%20S%20FRANCHISES%20DIST_%20HOCCO%20ICE%20CREAM!5e0!3m2!1sen!2sin!4v1737611430160!5m2!1sen!2sin"],
+    startDate: "15/11/2023",
+    locations: [
+      {
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117524.7896871604!2d72.3536168972656!3d22.99990630000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9b00149f940d%3A0x7f561de6c03092fb!2sH%20%26%20S%20FRANCHISES%20DIST_%20HOCCO%20ICE%20CREAM!5e0!3m2!1sen!2sin!4v1737611430160!5m2!1sen!2sin",
+        locationName: "Makarba",
+      },
+    ],
     investment: "₹2Cr - ₹3Cr",
     revenue: "₹6Cr average annual turnover",
     employeeCount: "2-3 per location",
@@ -49,23 +63,30 @@ const franchiseData = {
     name: "Chhaswala",
     hero: "/brands/chhaswala.jpg",
     description: "India's fast growing dairy brand",
-    yearsManaged: "5Months+",
-    locations: ["https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29373.62647752157!2d72.51418504520062!3d23.034663360732683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9bee2a90484d%3A0x9bc1bd26afbe91fa!2sChhaswala-%20iscon!5e0!3m2!1sen!2sin!4v1737611567855!5m2!1sen!2sin"],
+    startDate: "1/08/2024",
+    locations: [
+      {
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29373.62647752157!2d72.51418504520062!3d23.034663360732683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9bee2a90484d%3A0x9bc1bd26afbe91fa!2sChhaswala-%20iscon!5e0!3m2!1sen!2sin!4v1737611567855!5m2!1sen!2si",
+        locationName: "Satellite",
+      },
+    ],
     investment: "₹150K - ₹350K",
     revenue: "₹1Cr average annual turnover",
     employeeCount: "2 per location",
-    gallery: [
-      "/franchises/pigeon/Pigeon_inside.jpg",
-      "/franchises/pigeon/Pigeon.jpg",
-    ],
+    gallery: ["/franchises/chhaswala/chhaswala.jpg"],
   },
   pigeon: {
     name: "Pigeon",
     hero: "/brands/pigeon-2.jpg",
     description:
       "Pigeon is a Leader in cookware, home and kitchen appliances products across multiple categories",
-    yearsManaged: "4Months+",
-    locations: ["https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d234941.68002458225!2d72.36730551315607!3d23.06179052057008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e85630d30d745%3A0x5a43319ccb8facc9!2sPigeon%20Exclusive%20Store%20-%20Bhimjipura%2C%20Ahmedabad!5e0!3m2!1sen!2sin!4v1737611903173!5m2!1sen!2sin"],
+      startDate: "1/09/2024",
+      locations: [
+      {
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d234941.68002458225!2d72.36730551315607!3d23.06179052057008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e85630d30d745%3A0x5a43319ccb8facc9!2sPigeon%20Exclusive%20Store%20-%20Bhimjipura%2C%20Ahmedabad!5e0!3m2!1sen!2sin!4v1737611903173!5m2!1sen!2sin",
+        locationName: "Bhimjipura",
+      },
+    ],
     investment: "₹500K - ₹550K",
     revenue: "₹1Cr average annual turnover",
     employeeCount: "3-4 per location",
@@ -75,7 +96,6 @@ const franchiseData = {
     ],
   },
 };
-
 
 function FranchisePage() {
   const { slugs } = useParams();
@@ -106,7 +126,7 @@ function FranchisePage() {
           <div className="absolute inset-0 bg-black bg-opacity-50" />
         </div>
         <div className="relative h-full flex items-center justify-center text-center">
-          <div className="text-white px-4">
+          <div className="mt-20 text-white px-4">
             <h1 className="text-5xl tracking-[.1em] font-bold mb-4 uppercase text-white">
               {franchise.name}
             </h1>
@@ -120,8 +140,8 @@ function FranchisePage() {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           <div className="bg-white p-6 rounded-lg shadow-md">
             <Calendar1Icon className="w-8 h-8 text-blue-500 mb-4" />
-            <h3 className="text-lg font-semibold mb-2">Years Managed</h3>
-            <p className="text-gray-600">{franchise.yearsManaged}</p>
+            <h3 className="text-lg font-semibold mb-2">Since</h3>
+            <p className="text-gray-600">{franchise.startDate}</p>
           </div>
           <div className="bg-white p-6 rounded-lg shadow-md">
             <IndianRupee className="w-8 h-8 text-green-500 mb-4" />
@@ -143,32 +163,39 @@ function FranchisePage() {
 
       {/* Locations Map Section */}
       <div className="max-w-7xl mx-auto pb-16 px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-bold mb-8">Our Locations</h2>
+        <h2 className="text-3xl font-bold mb-4 flex align-middle gap-3">
+          <Store size={35} /> Our Franchise Locations
+        </h2>
         <div className="flex flex-col gap-4">
           {franchise?.locations?.map((item, index) => (
-            <a href="https://www.google.com" rel="noreferrer" key={index}>
+            <div key={index}>
+              <div className="my-4 flex gap-2 justify-start">
+                <MapPin /> {item.locationName}
+              </div>
               <div className="bg-white p-4 rounded-lg shadow-md h-[50vh]">
                 <iframe
-                  src={item}
+                  src={item.url}
                   allowFullScreen=""
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
                   className="w-full h-full"
                 ></iframe>
               </div>
-            </a>  
+            </div>
           ))}
         </div>
       </div>
 
       {/* Gallery Section */}
       <div className="max-w-7xl mx-auto pb-16 px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-bold mb-8">Gallery</h2>
+        <h2 className="text-3xl font-bold mb-4 flex align-middle gap-3">
+          <GalleryHorizontalEnd size={35} /> Gallery
+        </h2>{" "}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
           {franchise.gallery.map((image, index) => (
             <div
               key={index}
-              className="aspect-w-16 aspect-h-9 rounded-lg overflow-hidden"
+              className="aspect-w-16 aspect-h-9 max-h-[600px] rounded-lg overflow-hidden"
             >
               <img
                 src={image}
